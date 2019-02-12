df960e87b4bd5b4afd26c055a4128f0683170581
dingcuidingcui <json_dingcui@163.com>
2019-02-12 20:10:23 +0800

    王耀杰项目

---
+++ vue-webApp/vue-app/src/pages/Error/Error.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="error top">
+      <HeaderTop title="网页走丢了。。。"/>>
+    
+      <img src="./timg.jpg" alt="">
+    </div>
+</template>
+
+<script>
+ import HeaderTop from '../../components/HeaderTop/HeaderTop.vue'
+    export default {
+        name: "Error",
+      components: {HeaderTop}
+    }
+</script>
+
+<style scoped lang="scss">
+.error{
+  text-align: center;
+  img{
+    display: inline-block;
+    width: 100%;
+  }
+}
+</style>
